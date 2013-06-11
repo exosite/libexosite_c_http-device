ff3db1645ac0442b97646835307fa81b8cd3a5a1
Andy Lee <andylee@exosite.com>
2013-06-11 16:00:14 -0500

    Updating docgen script

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Create *.png files from text @startuml code and output *.png images to ./doc-src/images folder
 # Recusively search from current folder scanning *.c, *.cpp, *.h, and *.dox files
-java -jar /usr/share/plantuml/plantuml.jar -v -o $PWD/doc-src/images  "./**.(c|cpp|dox|h)"
+java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar -v -o html/  "./**.(c|cpp|dox|h)"
 doxygen Doxyfile
