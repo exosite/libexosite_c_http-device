8391336f79e2e804d99ab73706b537bda99f619f
Andy Lee <andylee@exosite.com>
2013-06-17 07:48:45 -0500

    Adding test for nvm clear

--- exosite-tests/src/tests.cpp
+++ exosite-tests/src/tests.cpp
@@ -21,6 +21,7 @@ virtual void SetUp() {
 };
 
 
+// Checks the read and write CIK functions
 TEST_F(ExoLibCleanNVM, cikRW)
 {
     char testcik[41] = "abcdef1234abcdef1234abcdef1234abcdef1234";
@@ -28,6 +29,29 @@ TEST_F(ExoLibCleanNVM, cikRW)
     char readResults[41] = "";
     exoHAL_meta_read(readResults,EXO_META_CIK);
     EXPECT_STREQ(testcik,readResults);
+
+
+}
+
+
+// Checks that the nvmClear actually clears out NVM
+TEST_F(ExoLibCleanNVM, nvmClear)
+{
+    mem.cik = "abcdef1234abcdef1234abcdef1234abcdef1234";
+    mem.model = "myModelName";
+    mem.server = "myServerName";
+    mem.uuid = "aUUID";
+    mem.vendor = "myVendorName";
+
+
+    exoHAL_EraseMeta();
+    
+    EXPECT_STREQ(mem.cik, "");
+    EXPECT_STREQ(mem.model, "");
+    EXPECT_STREQ(mem.server, "");
+    EXPECT_STREQ(mem.uuid, "");
+    EXPECT_STREQ(mem.vendor, "");
+    
 
 
 }
