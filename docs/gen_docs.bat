bd09ae6dba8ed4b4c74a2acdb5afe931798c741a
Andy Lee <andylee@exosite.com>
2013-06-17 10:58:08 -0500

    Updating Doxygen for new paths
    Updating so Doxy files to new source directories.

--- docs/gen_docs.bat
+++ docs/gen_docs.bat
@@ -8,10 +8,21 @@ SET DOXYGEN_EXE="C:\Program Files\doxygen\bin\doxygen.exe"
 SET PLANTUML_JAR=C:\inPath\plantuml.jar
 
 :: set source dir
-SET SOURCE_DIR=..\src
+SET SOURCE_DIR=..\exosite-lib\src
 
 :: set image output folder for plantuml / image source folder for doxygen
-SET DOC_IMG_PATH=..\docs\html\
+SET DOC_IMG_PATH=..\..\docs\html\
+
+:: change to source dir and save old dir on stack
+PUSHD "%SOURCE_DIR%"
+
+:: call plantuml.jar for current dir
+java -jar %PLANTUML_JAR% -v -o %DOC_IMG_PATH% "./**.(c|cpp|dox|h)"
+
+:: change to saved dir
+POPD
+
+SET SOURCE_DIR=..\exosite-hal\src
 
 :: change to source dir and save old dir on stack
 PUSHD "%SOURCE_DIR%"
