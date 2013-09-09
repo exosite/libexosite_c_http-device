3d04586d771dae6bd0a42e48e4786c8e5f554da5
Andy Lee <andylee@exosite.com>
2013-09-09 16:38:13 -0500

    Updated plantuml output image path

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Create *.png files from text @startuml code and output *.png images to ./doc-src/images folder
 # Recusively search from current folder scanning *.c, *.cpp, *.h, and *.dox files
-java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar -v -o "../docs/html/"  "../exosite-lib/src/exosite_c_libraries.dox"
+java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar -v -o "../../docs/html/"  "../exosite-lib/src/exosite_c_libraries.dox"
 doxygen Doxyfile
