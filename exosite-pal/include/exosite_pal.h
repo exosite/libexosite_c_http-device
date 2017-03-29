c2d0298b693818ea3e4034377d235c5e653f3c9a
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-29 11:02:49 -0500

    Separate common PAL header parts from private parts.
    The common part is what all PAL are expected to implement.
    
    The private part is for the implementation specific details.

---
+++ exosite-pal/include/exosite_pal.h
@@ -0,0 +1,63 @@
+/*****************************************************************************
+*
+*  exosite_hal.h - Common header for Exosite hardware adapation layer
+*  Copyright (C) 2012-2017 Exosite LLC
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*    Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*
+*    Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the
+*    distribution.
+*
+*    Neither the name of Exosite LLC nor the names of
+*    its contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+
+#ifndef EXOSITE_PAL_H
+#define EXOSITE_PAL_H
+#include <stdint.h>
+
+// functions for export
+void exoPal_init();
+uint8_t exoPal_setCik(const char * read_buffer);
+uint8_t exoPal_getCik(char * read_buffer);
+uint8_t exoPal_getModel(char * read_buffer);
+uint8_t exoPal_getVendor(char * read_buffer);
+uint8_t exoPal_getUuid(char * read_buffer);
+
+uint8_t exoPal_tcpSocketClose();
+uint8_t exoPal_tcpSocketOpen();
+uint8_t exoPal_socketRead( char * buffer, uint16_t bufSize, uint16_t * responseLength);
+uint8_t exoPal_socketWrite( const char * buffer, uint16_t len);
+int32_t exoPal_sendingComplete();
+
+uint8_t exoPal_itoa(int value, char* buf, uint8_t bufSize);
+int32_t exoPal_atoi(char* val);
+uint16_t exoPal_strlen(const char *s);
+char* exoPal_strstr(const char *str, const char *target);
+void * exoPal_memcpy(void* dst, const void * src, uint16_t length);
+
+
+#endif
+
+
--- exosite-pal/pal_template/include/exosite_pal_private.h
+++ exosite-pal/pal_template/include/exosite_pal_private.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
 *
 *  exosite_hal.h - Common header for Exosite hardware adapation layer
-*  Copyright (C) 2012 Exosite LLC
+*  Copyright (C) 2012-2017 Exosite LLC
 *
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
@@ -33,8 +33,8 @@
 *
 *****************************************************************************/
 
-#ifndef EXOSITE_HAL_H
-#define EXOSITE_HAL_H
+#ifndef EXOSITE_PAL_PRIVATE_H
+#define EXOSITE_PAL_PRIVATE_H
 #include <stdint.h>
 
 
@@ -45,22 +45,8 @@ to place incoming data from the modem in.*/
 
 extern char  exoPal_rxBuffer[RX_BUFFER_SIZE];
 
-// functions for export
-void exoPal_init();
-uint8_t exoPal_setCik(const char * read_buffer);
-uint8_t exoPal_getCik(char * read_buffer);
-uint8_t exoPal_getModel(char * read_buffer);
-uint8_t exoPal_getVendor(char * read_buffer);
-uint8_t exoPal_getUuid(char * read_buffer);
 
-uint8_t exoPal_tcpSocketClose();
-uint8_t exoPal_tcpSocketOpen();
-uint8_t exoPal_socketRead( char * buffer, uint16_t bufSize, uint16_t * responseLength);
-uint8_t exoPal_socketWrite( const char * buffer, uint16_t len);
-
-uint8_t exoPal_itoa(int value, char* str, uint8_t radix);
-uint16_t exoPal_strlen(const char *s);
-uint8_t exoPal_memcpy(char * dst, const char * src, uint16_t length);
+#include "exosite_pal.h"
 
 
 #endif
