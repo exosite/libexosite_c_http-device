f31a306d95505717a88e4e5b63b9bdd841bdc553
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-11-14 10:13:58 -0600

    Expect plantuml to be installed

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Create *.png files from text @startuml code and output *.png images to ./doc-src/images folder
 # Recusively search from current folder scanning *.c, *.cpp, *.h, and *.dox files
-java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar -v -o "../../docs/html/"  "../exosite-lib/src/exosite_c_libraries.dox"
+
+plantuml -v -o "../../docs/html/"  "../exosite-lib/src/exosite_c_libraries.dox"
+
 doxygen Doxyfile
+
